933a1b1dffe49f0223212a3484fc78af35f7756f
kolya-makarenko <lifestealer27@gmail.com>
2025-04-16 03:40:10 +0300

    Added a mobile version of the header, homepage, and services

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -1,30 +1,46 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router';
 
 import classes from './Menu.module.css';
 
 const Menu = () => {
+    const [isMenuActive, setIsMenuActive] = useState(false);
+
+    const handleClick = () => {
+        setIsMenuActive(!isMenuActive);
+    };
+
     return (
-        <nav className={classes.mainMenu}>
+        <nav
+            className={`${classes.mainMenu} ${
+                isMenuActive ? classes.menuActive : ''
+            }`}
+        >
             <ul>
-                <li>
+                <li onClick={handleClick}>
                     <NavLink to="/">Головна</NavLink>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                     <NavLink to="/services">Послуги</NavLink>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                     <NavLink to="/about">Про мене</NavLink>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                     <NavLink to="/galery">Галерея робіт</NavLink>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                     <NavLink to="/contacts">Контакти</NavLink>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                     <NavLink to="/form">Запис</NavLink>
                 </li>
             </ul>
+            <div className={classes.burgerMenu} onClick={handleClick}>
+                <div className={classes.burgerLine1}></div>
+                <div className={classes.burgerLine2}></div>
+                <div className={classes.burgerLine3}></div>
+            </div>
         </nav>
     );
 };
